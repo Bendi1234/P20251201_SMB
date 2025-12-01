76cfce1ebcdf1141ea0962efe8bb53ab931c50de
Bendi1234 <matebendi0304@gmail.com>
2025-12-01 14:06:16 +0100

    #2

--- P20251201_SMB/UnitTest1.cs
+++ P20251201_SMB/UnitTest1.cs
@@ -5,16 +5,41 @@ namespace P20251201_SMB
         [SetUp]
         public void Setup()
         {
-            /*Console.WriteLine("E-mail: ");
-           string jelszo=Convert.ToString(Console.ReadLine());*/
 
+            string jelszo = "Qwertz1234";
+            int jelszohossz=jelszo.Length;
         }
 
         [Test]
         public void Test1()
         {
            StringAssert.Contains("@","pelda@gmail.com");
+           
 
+
+        }
+        [Test]
+        public void Test2()
+        {
+            StringAssert.Contains("1234", "pelda@gmail.com");
+        }
+        [Test]
+        public void Test3()
+        {
+            StringAssert.EndsWith(".com", "pelda@gmail.hu");
+        }
+        [Test]
+        public void Test4()
+        {
+            StringAssert.StartsWith("+36", "+36301213453");
+        }
+        [Test]
+        public void Test5()
+        {
+            string jelszo = "Qwertz1234";
+            int jelszohossz = jelszo.Length;
+
+            Assert.Equals(jelszohossz,10);
         }
     }
 }
