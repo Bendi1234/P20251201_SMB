a23b17d917fd969d739c7ee9a60bf1dae9a4c40d
Bendi1234 <matebendi0304@gmail.com>
2025-12-01 13:56:00 +0100

    #1

--- P20251201_SMB/UnitTest1.cs
+++ P20251201_SMB/UnitTest1.cs
@@ -5,12 +5,16 @@ namespace P20251201_SMB
         [SetUp]
         public void Setup()
         {
+            /*Console.WriteLine("E-mail: ");
+           string jelszo=Convert.ToString(Console.ReadLine());*/
+
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+           StringAssert.Contains("@","pelda@gmail.com");
+
         }
     }
 }
