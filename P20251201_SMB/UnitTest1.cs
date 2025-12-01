0d60d976b83e8faa0ddd456963cc2025300b3ec3
Bendi1234 <matebendi0304@gmail.com>
2025-12-01 13:42:55 +0100

    Add project files.

---
+++ P20251201_SMB/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace P20251201_SMB
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
